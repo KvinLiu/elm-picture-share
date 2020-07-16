c56526cd8b1610b77956d9cf750de8957c825961
Kevin Liu <kevin07liu@gmail.com>
2020-07-16 15:14:07 +0800

    Add comment feature and refactor the code

--- src/Picshare.elm
+++ src/Picshare.elm
@@ -2,8 +2,8 @@ module Picshare exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (class, src)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (class, disabled, placeholder, src, type_, value)
+import Html.Events exposing (onClick, onInput, onSubmit)
 
 
 baseUrl : String
@@ -11,20 +11,29 @@ baseUrl =
     "https://programming-elm.com/"
 
 
+type Msg
+    = ToggleLike
+    | UpdateComment String
+    | SaveComment
+
+
 type alias Model =
     { url : String
     , caption : String
     , liked : Bool
+    , comments : List String
+    , newComment : String
     }
-
-
-type Msg
-    = ToggleLike
 
 
 initialModel : Model
 initialModel =
-    Model (baseUrl ++ "1.jpg") "Surfing!" False
+    { url = baseUrl ++ "1.jpg"
+    , caption = "Surfing!"
+    , liked = False
+    , comments = [ "Cowabunga, dude!" ]
+    , newComment = ""
+    }
 
 
 viewLoveButton : Model -> Html Msg
@@ -39,6 +48,41 @@ viewLoveButton model =
     in
     div [ class "like-button" ]
         [ i [ class "fa fa-2x", class buttonClass, onClick ToggleLike ] []
+        ]
+
+
+viewComment : String -> Html Msg
+viewComment comment =
+    li []
+        [ strong [] [ text "Comment:" ]
+        , text (" " ++ comment)
+        ]
+
+
+viewCommentList : List String -> Html Msg
+viewCommentList comments =
+    case comments of
+        [] ->
+            text ""
+
+        _ ->
+            div [ class "comments" ] [ ul [] (List.map viewComment comments) ]
+
+
+viewComments : Model -> Html Msg
+viewComments model =
+    div []
+        [ viewCommentList model.comments
+        , form [ class "new-comment", onSubmit SaveComment ]
+            [ input
+                [ type_ "text"
+                , placeholder "Add a comment..."
+                , value model.newComment
+                , onInput UpdateComment
+                ]
+                []
+            , button [ disabled (String.isEmpty model.newComment) ] [ text "Save" ]
+            ]
         ]
 
 
@@ -57,6 +101,7 @@ viewDetailedPhoto model =
         , div [ class "photo-info" ]
             [ viewLoveButton model
             , h2 [ class "caption" ] [ text model.caption ]
+            , viewComments model
             ]
         ]
 
@@ -70,11 +115,34 @@ view model =
         ]
 
 
+saveNewComment : Model -> Model
+saveNewComment model =
+    let
+        comment =
+            String.trim model.newComment
+    in
+    case comment of
+        "" ->
+            model
+
+        _ ->
+            { model
+                | comments = model.comments ++ [ comment ]
+                , newComment = ""
+            }
+
+
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         ToggleLike ->
             { model | liked = not model.liked }
+
+        UpdateComment comment ->
+            { model | newComment = comment }
+
+        SaveComment ->
+            saveNewComment model
 
 
 main : Program () Model Msg
