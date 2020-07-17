eb0d3c6c9281638e3bb8329edd7e80bb7eebb50a
Kevin Liu <kevin07liu@gmail.com>
2020-07-17 22:48:05 +0800

    Update the Model and Subscriptions, Queue Photos

--- src/Picshare.elm
+++ src/Picshare.elm
@@ -5,7 +5,7 @@ import Html exposing (..)
 import Html.Attributes exposing (class, disabled, placeholder, src, type_, value)
 import Html.Events exposing (onClick, onInput, onSubmit)
 import Http
-import Json.Decode exposing (Decoder, bool, int, list, string, succeed)
+import Json.Decode exposing (Decoder, bool, decodeString, int, list, string, succeed)
 import Json.Decode.Pipeline exposing (hardcoded, required)
 import WebSocket
 
@@ -25,7 +25,8 @@ type Msg
     | UpdateComment Id String
     | SaveComment Id
     | LoadFeed (Result Http.Error Feed)
-    | LoadStreamPhoto String
+    | LoadStreamPhoto (Result Json.Decode.Error Photo)
+    | FlushStreamQueue
 
 
 type alias Id =
@@ -49,6 +50,7 @@ type alias Feed =
 type alias Model =
     { feed : Maybe Feed
     , error : Maybe Http.Error
+    , streamQueue : Feed
     }
 
 
@@ -67,6 +69,7 @@ initialModel : Model
 initialModel =
     { feed = Nothing
     , error = Nothing
+    , streamQueue = []
     }
 
 
@@ -213,7 +216,8 @@ saveNewComment photo =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    WebSocket.receive LoadStreamPhoto
+    WebSocket.receive
+        (LoadStreamPhoto << decodeString photoDecoder)
 
 
 toggleLike : Photo -> Photo
@@ -272,11 +276,15 @@ update msg model =
             , Cmd.none
             )
 
-        LoadStreamPhoto data ->
-            let
-                _ =
-                    Debug.log "WebSocket data" data
-            in
+        LoadStreamPhoto (Ok photo) ->
+            ( { model | streamQueue = photo :: model.streamQueue }
+            , Cmd.none
+            )
+
+        LoadStreamPhoto (Err _) ->
+            ( model, Cmd.none )
+
+        FlushStreamQueue ->
             ( model, Cmd.none )
 
 
